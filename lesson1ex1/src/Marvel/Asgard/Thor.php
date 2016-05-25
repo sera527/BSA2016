04c8bfbb2f289b4e4df70c56e442731acc25be56
Сергій <sergiy.posternak@iskyweb.com>
2016-05-26 00:00:29 +0300

    Initial commit

---
+++ lesson1ex1/src/Marvel/Asgard/Thor.php
@@ -0,0 +1,12 @@
+<?php
+namespace Marvel\Asgard;
+class Thor {
+    public static function whoami() {
+        $names = __CLASS__;
+        $names = explode("\\", $names);
+        $count = count($names);
+        return "I am ".$names[$count-1]." from ".$names[$count-2].".\n";
+    }
+}
+
+echo Thor::whoami();
---
+++ lesson1ex1/src/Marvel/Elmsford/HenryJonathanPym.php
@@ -0,0 +1,12 @@
+<?php
+namespace Marvel\Elmsford;
+class HenryJonathanPym {
+    public static function whoami() {
+        $names = __CLASS__;
+        $names = explode("\\", $names);
+        $count = count($names);
+        return "I am ".$names[$count-1]." from ".$names[$count-2].".\n";
+    }
+}
+
+echo HenryJonathanPym::whoami();
